1d617e49c0d8bf78030d4e71f750b0969e357d18
Parker Helland <hellandp@gmail.com>
2019-04-13 19:01:32 -0700

    bad practice

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ function start(route, handle) {
     const pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname);
-
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello Wurld');
+    const content = route(handle, pathname);
+    response.write(content);
     response.end();
   }
-  
+
   http.createServer(onRequest).listen(8888);
-  console.log('Server has started ∆');
+  console.log('∆∆∆ Server has started ∆∆∆');
 }
 
 exports.start = start;
